8933f1ff9ab6b0d6fc200b541e24937cdc62fbc0
Gonzalopetosa <gonzalopetosa1234@gmail.com>
2025-07-14 17:18:30 -0300

    model fix

--- models.py
+++ models.py
@@ -9,11 +9,9 @@ class CV(db.model):
     path = db.Column(db.Text, nullable=False)
     text = db.Column(db.Text, nullable=False)
     frecuent_words = db.Column(db.Text, nullable=False)
-    #FOREIGN KEY RELACION ENTRE CV--CANDIDATOS_APTO
-    id_candidato = db.Column(db.Integer, db.ForeignKey('candidato.id') ,nullable=False)
 
     def __repr__(self):
-        return f'<CV id={self.id} candidate={self.id_candidato} path={self.path}>'
+        return f'<CV id={self.id} path={self.path}>'
 
     def to_dict(self):
 
@@ -21,8 +19,7 @@ class CV(db.model):
             'id': self.id,
             'path': self.path,
             'text': self.text,
-            'frecuent_words': self.frecuent_words,
-            'id_candidato': self.id_candidato
+            'frecuent_words': self.frecuent_words
         }
 
 class Candidato_apto(db.Model):
@@ -35,7 +32,7 @@ class Candidato_apto(db.Model):
     telefono = db.Column(db.String(20), nullable=False)
     email = db.Column(db.Text, nullable=False)
     direccion = db.Column(db.Text, nullable=False)
-
+    id_cv = db.Column(db.Integer, db.ForeignKey('cv.id') ,nullable=False)
     def __repr__(self):
         return f'<Candidato id={self.id} nombre={self.nombre} {self.apellido}>'
 
@@ -47,7 +44,8 @@ class Candidato_apto(db.Model):
             'apellido': self.apellido,
             'telefono': self.telefono,
             'email': self.email,
-            'direccion': self.direccion
+            'direccion': self.direccion,
+            'id_cv': self.id_cv
         }
 
 class Oferta(db.Model):
@@ -58,11 +56,11 @@ class Oferta(db.Model):
     puesto = db.Column(db.String(12), nullable=False)
     etiquetas = db.Column(db.Text)
     id_reclutador = db.Column(db.Integer, db.ForeignKey('reclutador.id'))
-    id_candidato_apto = db.Column(db.Integer, db.ForeignKey('candidato_apto.id'))
+
 
 
     def __repr__(self):
-        return f'<JobPosition id={self.id} puesto={self.puesto}>'
+        return f'<oferta id={self.id} puesto={self.puesto}>'
 
     def to_dict(self):
 
@@ -70,8 +68,7 @@ class Oferta(db.Model):
             'id': self.id,
             'puesto': self.puesto,
             'etiquetas': self.etiquetas,
-            'id_reclutador': self.id_reclutador,
-            'id_candidato_apto': self.id_candidato_apto
+            'id_reclutador': self.id_reclutador
         }
 
 class Reclutador(db.Model):
@@ -84,7 +81,7 @@ class Reclutador(db.Model):
     oferta = db.Column(db.Integer, db.ForeignKey('oferta.id'))
 
     def __repr__(self):
-        return f'<Recruiter id={self.id} nombre={self.nombre}>'
+        return f'<reclutador id={self.id} nombre={self.nombre}>'
 
     def to_dict(self):
 
@@ -92,4 +89,23 @@ class Reclutador(db.Model):
             'id': self.id,
             'nombre': self.nombre,
             'oferta': self.oferta
+        }
+
+class Candidatos_oferta(db.Model):
+
+    __tablename__ = 'candidatos_oferta'
+
+    id = db.Column(db.Integer, Sequence('candidato_oferta'), primary_key=True)
+    id_candidatoapto = db.Column(db.Integer, db.ForeignKey('candidato_apto.id') ,nullable=False)
+    id_oferta = db.Column(db.Integer, db.ForeignKey('oferta.id') ,nullable=False)
+
+    def __repr__(self):
+        return f'<candidato_id={self.id_candidatoapto} oferta_id={self.id_oferta}>'
+
+    def to_dict(self):
+
+        return {
+            'id': self.id,
+            'id_candidatoapto': self.id_candidatoapto,
+            'id_oferta': self.id_oferta
         }
