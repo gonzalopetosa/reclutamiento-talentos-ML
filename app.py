c753bb2d3ef6af7ed268b39cc2cf52852feb9009
Gonzalopetosa <gonzalopetosa1234@gmail.com>
2025-07-17 22:31:12 -0300

    implement CORS to front-end

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import request, Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import os
 from modelos.models import db
+from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
 
@@ -9,6 +11,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhos
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+CORS(app)
 
 from rutas.oferta import oferta_bp
 from rutas.reclutador import reclutador_bp
@@ -17,5 +20,6 @@ from rutas.candidato_apto import candidato_apto_bp
 app.register_blueprint(oferta_bp, url_prefix='/oferta')
 app.register_blueprint(reclutador_bp, url_prefix='/reclutador')
 app.register_blueprint(candidato_apto_bp, url_prefix='/candidatos_aptos')
+
 if __name__== '__main__':
     app.run(port=int(os.environ.get("FLASK_PORT", 5000)))
